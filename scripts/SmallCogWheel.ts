9d758a85e869b432584127390de766dcea16fcd9
sanaehururu1200 <sanaehururu1200@gmail.com>
2023-08-19 03:39:14 +0900

    SideがTopで上書きされたりするのが原因ぽい

--- scripts/SmallCogWheel.ts
+++ scripts/SmallCogWheel.ts
@@ -30,36 +30,6 @@ export class SmallCogWheel extends BlockEntity implements Rotatable, Tickable {
     entities.forEach((entity) => {
       if (entity != null) {
         if (entity.typeId.toString() == "infracraft:small_cog_wheel") {
-          world
-            .getDimension("overworld")
-            .getEntitiesAtBlockLocation(new Vector(entity.location.x, entity.location.y + 1, entity.location.z))
-            .forEach((topEntity) => {
-              try {
-                var topRPM: string | number | boolean | Vector3 = topEntity.getDynamicProperty("rpm") ?? 0;
-                if (typeof topRPM === "string") {
-                  topRPM = parseInt(topRPM);
-                }
-                if (typeof topRPM === "number") {
-                  if (topRPM != 0) {
-                    this.RPM = topRPM;
-                    entity.setDynamicProperty("rpm", this.RPM);
-                    entity.teleport(
-                      { x: entity.location.x, y: entity.location.y, z: entity.location.z },
-                      {
-                        rotation: { x: 0, y: entity.getRotation().y + this.RPM * 18 },
-                      }
-                    );
-                    world.getAllPlayers().forEach((player) => player.sendMessage("top:" + topRPM.toString()));
-                    return;
-                  }
-                } else {
-                  world.getAllPlayers().forEach((player) => player.sendMessage("top:zero"));
-                }
-              } catch (e: any) {
-                world.getAllPlayers().forEach((player) => player.sendMessage(e.toString()));
-              }
-            });
-
           let sides: Vector[] = [
             new Vector(entity.location.x, entity.location.y, entity.location.z + 1),
             new Vector(entity.location.x, entity.location.y, entity.location.z - 1),
@@ -69,11 +39,14 @@ export class SmallCogWheel extends BlockEntity implements Rotatable, Tickable {
           var sideRPMArray: number[] = [];
           sides.forEach((side) => {
             var max = 0;
+            var count: number = 0;
             world
               .getDimension("overworld")
               .getEntitiesAtBlockLocation(side)
               .forEach((sideEntity) => {
                 try {
+                  world.getAllPlayers().forEach((player) => player.sendMessage("count:" + count.toString()));
+                  count++;
                   var sideRPM: string | number | boolean | Vector3 = sideEntity.getDynamicProperty("rpm") ?? 0;
                   if (typeof sideRPM === "string") {
                     sideRPM = parseInt(sideRPM);
@@ -86,12 +59,16 @@ export class SmallCogWheel extends BlockEntity implements Rotatable, Tickable {
                     sideRPM = 0;
                     world.getAllPlayers().forEach((player) => player.sendMessage("zero"));
                   }
-                  if (max < sideRPM) max = sideRPM;
+                  if (max < sideRPM) {
+                    max = sideRPM;
+                    world.getAllPlayers().forEach((player) => player.sendMessage("max set to " + max.toString()));
+                  }
                 } catch (e: any) {
                   world.getAllPlayers().forEach((player) => player.sendMessage(e.toString()));
                 }
               });
             this.RPM = -max;
+            //if (this.RPM >= 1)
             entity.setDynamicProperty("rpm", this.RPM);
             entity.teleport(
               { x: entity.location.x, y: entity.location.y, z: entity.location.z },
@@ -103,6 +80,37 @@ export class SmallCogWheel extends BlockEntity implements Rotatable, Tickable {
               world.playSound("block.scaffolding.climb", entity.location);
             }
           });
+
+          world
+            .getDimension("overworld")
+            .getEntitiesAtBlockLocation(new Vector(entity.location.x, entity.location.y + 1, entity.location.z))
+            .forEach((topEntity) => {
+              try {
+                var topRPM: string | number | boolean | Vector3 = topEntity.getDynamicProperty("rpm") ?? 0;
+                if (typeof topRPM === "string") {
+                  topRPM = parseInt(topRPM);
+                }
+                if (typeof topRPM === "number") {
+                  if (topRPM != 0) {
+                    this.RPM = topRPM;
+                    if (this.RPM != 0) {
+                      entity.setDynamicProperty("rpm", this.RPM);
+                      entity.teleport(
+                        { x: entity.location.x, y: entity.location.y, z: entity.location.z },
+                        {
+                          rotation: { x: 0, y: entity.getRotation().y + this.RPM * 18 },
+                        }
+                      );
+                      world.getAllPlayers().forEach((player) => player.sendMessage("top:" + topRPM.toString()));
+                    }
+                  }
+                } else {
+                  world.getAllPlayers().forEach((player) => player.sendMessage("top:zero"));
+                }
+              } catch (e: any) {
+                world.getAllPlayers().forEach((player) => player.sendMessage(e.toString()));
+              }
+            });
         }
       }
     });
